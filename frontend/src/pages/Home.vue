3bd3ea4788fd84c903c3457d07eb451098970d29
janka <nikolajankovicjanka@gmail.com>
2026-07-23 11:45:17 +0200

    feat: Reklamni popup banner.

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -16,7 +16,7 @@
         </div>
     </section>
     <SeasonTicketPopup
-        image-src="/hero/fk-radnik-sezonske.jpg"
+        image-src="/hero/fk-radnik-sezonske.png"
         title="Sezonske karte u prodaji"
         subtitle="Osiguraj svoje mjesto na tribinama. Izaberi paket i podrži FK Radnik."
         badge-text="2026/27"
@@ -32,7 +32,7 @@
 
 
   <SeasonTicketsPromo
-        image-src="/hero/fk-radnik-sezonske.jpg"
+        image-src="/hero/fk-radnik-sezonske-banner.png"
         title="Sezonske karte u prodaji"
         subtitle="Osiguraj svoje mjesto na tribinama. Izaberi paket i podrži FK Radnik."
         badge-text="2026/27"
